84cf6bbf4f52836ba22a505dbe3ac668942dc9e9
patelvijayg <patelvijayg@gmail.com>
2018-11-09 14:10:28 +0530

    changes for inserting dummynode

--- Arango_SampleDB_create.js
+++ Arango_SampleDB_create.js
@@ -43,21 +43,53 @@ child = exec('dir',
 
 function testing(){
 
-db._useDatabase(DBNAME)
-var cities = db._create("cities");
-var roads = db._createEdgeCollection("roads");
-//var mumbai = cities.save({_key: "mumbai", name:"mumbai", state:"mh"})
-//var pune = cities.save({_key: "pune", name:"pune", state:"mh"})
-//var nagpur = cities.save({_key: "nagpur", name:"nagpur", state:"mh"})
+	db._useDatabase(DBNAME)
+	var cities = db._create("cities");
+	var roads = db._createEdgeCollection("roads");
+	
+	var nodeinsert=[
+	{"_key":"1","field1":"A","range":1,"delta":[{"a":"1"}]},
+	{"_key":"2","field1":"A","range":2,"delta":[]},
+	{"_key":"3","field1":"A","range":3,"delta":[]},
+	{"_key":"4","field1":"A","range":4,"delta":[{"a":"1"}]},
+	{"_key":"5","field1":"A","range":5,"delta":[{"a":"1"}]},
+	{"_key":"6","field1":"A","range":6,"delta":[]},
+	{"_key":"7","field1":"A","range":7,"delta":[]},
+	{"_key":"8","field1":"A","range":8,"delta":[]},
+	{"_key":"9","field1":"A","range":9,"delta":[{"a":"1"}]},
+	{"_key":"10","field1":"A","range":10,"delta":[{"a":"1"}]}
+	];
 
-//var nh5 = roads.save(mumbai,pune,{name:"express way 1",distance:200})
-//var nh6 = roads.save(mumbai,nagpur,{name:"express way 2",distance:600})
+	var edgeinsert=
+	[
+	{"_from":"dummynode/1","_to":"dummynode/2","desc":"1to2"},
+	{"_from":"dummynode/1","_to":"dummynode/3","desc":"1to3"},
+	{"_from":"dummynode/2","_to":"dummynode/4","desc":"2to4"},
+	{"_from":"dummynode/2","_to":"dummynode/5","desc":"2to5"},
+	{"_from":"dummynode/3","_to":"dummynode/6","desc":"3to6"},
+	{"_from":"dummynode/3","_to":"dummynode/7","desc":"3to7"},
+	{"_from":"dummynode/4","_to":"dummynode/8","desc":"4to8"},
+	{"_from":"dummynode/5","_to":"dummynode/9","desc":"5to9"},
+	{"_from":"dummynode/7","_to":"dummynode/10","desc":"7to10"}
+	];
+	var dummynode = db._create("dummynode");
+	db.dummynode.insert(nodeinsert);
+	
+	var dummyedge = db._createEdgeCollection("dummyedge");
+	db.dummyedge.insert(edgeinsert);
+	//var mumbai = cities.save({_key: "mumbai", name:"mumbai", state:"mh"})
+	//var pune = cities.save({_key: "pune", name:"pune", state:"mh"})
+	//var nagpur = cities.save({_key: "nagpur", name:"nagpur", state:"mh"})
 
-try{
-var Graph = require('@arangodb/general-graph');
-var g = Graph._create('road_graph',[Graph._relation('roads', ['cities'], ['cities'])]);
-{print("CREATING GRAPH")}
-}
-catch(e) {print("CREATING GRAPH .... error occur"+e)}
+	//var nh5 = roads.save(mumbai,pune,{name:"express way 1",distance:200})
+	//var nh6 = roads.save(mumbai,nagpur,{name:"express way 2",distance:600})
+
+	try{
+	var Graph = require('@arangodb/general-graph');
+	var g1 = Graph._create('road_graph',[Graph._relation('roads', ['cities'], ['cities'])]);
+	var g2 = Graph._create('dummygraph',[Graph._relation('dummyedge', ['dummynode'], ['dummynode'])]);
+	{print("CREATING GRAPH")}
+	}
+	catch(e) {print("CREATING GRAPH .... error occur"+e)}
 }
 
